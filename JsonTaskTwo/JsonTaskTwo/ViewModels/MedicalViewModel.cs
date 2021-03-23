e0c6820e871e5e668ad0dd073947a21a7fb9a593
GraceJuliet <71426771+GraceJuliet@users.noreply.github.com>
2021-03-24 00:02:09 +0530

    Add files via upload

---
+++ JsonTaskTwo/JsonTaskTwo/ViewModels/MedicalViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonTaskTwo.Models;
+using Newtonsoft.Json;
+
+namespace JsonTaskTwo.ViewModels
+{
+    public class MedicalViewModel : INotifyPropertyChanged
+    {
+
+        Medical ObjMedical;
+        Sections ObjSections;
+
+
+
+        public static Medical md = new Medical()
+        {
+
+        };
+
+
+        public static string strResultJson = JsonConvert.SerializeObject(md);
+       //Console.WriteLine(strResultJson);
+
+       // var str=File.WriteAllText(@"C:\Users\Grace Juliet\Desktop\WPF-C# Tasks\medical.json",strResultJson);
+        public MedicalViewModel()
+        {
+            ObjMedical = new Medical();
+
+            ObjSections = new Sections();
+
+
+
+
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
