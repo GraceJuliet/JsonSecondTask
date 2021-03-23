7c528e1790abeb5dca217dbe37df00bed1c948b1
GraceJuliet <71426771+GraceJuliet@users.noreply.github.com>
2021-03-24 00:00:33 +0530

    Add files via upload

---
+++ JsonTaskTwo/JsonTaskTwo/Models/Medical.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTaskTwo.Models
+{
+    public class Medical : INotifyPropertyChanged
+    {
+        private string sectionHeader;
+        public string SectionHeader
+        {
+            get
+            {
+                return sectionHeader;
+            }
+            set
+            {
+                sectionHeader = value;
+                OnPropertyChanged("SectionHeader");
+            }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        private string format;
+
+        public string Format
+        {
+            get
+            {
+                return Format;
+            }
+            set
+            {
+                Format = value;
+                OnPropertyChanged("Format");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string values;
+        public string Values
+        {
+            get
+            {
+                return values;
+            }
+            set
+            {
+                values = value;
+                OnPropertyChanged("Values");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ JsonTaskTwo/JsonTaskTwo/Models/Sections.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTaskTwo.Models
+{
+    public class Sections
+    {
+        public string Names { get; set; }
+        public string Title { get; set; }
+        public string Format { get; set; }
+        public string Name { get; set; }
+        public string Values { get; set; }
+    }
+}
